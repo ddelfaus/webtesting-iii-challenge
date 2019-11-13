98d2f7d0ea613389c9a906c18508187695e5fdc6
ddelfaus <ddelfaus@gmail.com>
2019-11-13 17:03:26 -0500

    added test to display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import Display from './Display'
+import { get } from 'http';
+import expectExport from 'expect';
 
 afterEach(cleanup)
 
@@ -18,5 +20,28 @@ test('App renders', () => {
     getByText(/Open/i);
   });
 
-   
 
+  test("It looks for the Green class when it is unlocked or open", () => {
+
+
+    const { getByText, rerender } = render(<Display locked={false} />);
+    getByText(/lock/i);
+    expect(getByText(/lock/i).classList.contains("green-led")).toBe(true);
+  
+    rerender(<Display closed={false} />);
+    getByText(/open/i);
+    expect(getByText(/open/i).classList.contains("green-led")).toBe(true);
+  });
+
+   
+test("It looks for the red class when it is locked or closed", () => {
+
+
+  const { getByText, rerender } = render(<Display locked={true} />);
+  getByText(/locked/i);
+  expect(getByText(/locked/i).classList.contains("red-led")).toBe(true);
+
+  rerender(<Display closed={true} />);
+  getByText(/closed/i);
+  expect(getByText(/closed/i).classList.contains("red-led")).toBe(true);
+});
